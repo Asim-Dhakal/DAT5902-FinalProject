1c5ae206cbc1b06324ad6c4fb1f6134b76be832b
Asim-Dhakal <asimdhakal444@gmail.com>
2025-01-01 21:55:47 +0000

    comment change

--- First.py
+++ First.py
@@ -8,8 +8,8 @@ data = pd.read_csv("global_air_pollution_data.csv")
 aqi_counts = data['aqi_category'].value_counts()
 
 # Plot the pie chart
-plt.figure(figsize=(8, 8))  # This line should work after reinstalling matplotlib
+plt.figure(figsize=(8, 8)) # figure size
 aqi_counts.plot(kind='pie', autopct='%1.1f%%', startangle=90, colormap='tab20')
 plt.title('Distribution of AQI Categories')
-plt.ylabel('')  # Remove default ylabel
+plt.ylabel('')  
 plt.show()
