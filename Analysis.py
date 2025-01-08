42d642c4ab1452538c6b2c0e9c27769f08383d53
Asim-Dhakal <asimdhakal444@gmail.com>
2025-01-08 01:11:36 +0000

    Box plot added

--- Analysis.py
+++ Analysis.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 Ourdata = ('global_air_pollution_data.csv')
 ## Pi chart for aqi 
 def plot_aqi_pie_chart(file_path):
-    # Load dataset
+    ## Load dataset
     data = pd.read_csv(file_path)
 
-    # Count the occurrences of each AQI category
+    ## Count the occurrences of each AQI category
     aqi_counts = data['aqi_category'].value_counts()
-    # Calculate the percentage for each category
+    ## Calculate the percentage for each category
     percentages = 100 * aqi_counts / aqi_counts.sum()
 
-    # pie chart
+    ## pie chart
     fig, ax = plt.subplots(figsize=(8, 8))  
     colors = plt.get_cmap('tab20')(range(len(aqi_counts))) 
     wedges, texts, autotexts = ax.pie(aqi_counts, 
@@ -22,11 +24,11 @@ def plot_aqi_pie_chart(file_path):
                                       labeldistance=1.2, 
                                       wedgeprops={'edgecolor': 'black'})
 
-    # Title and legend with percentage
+    ## Title and legend with percentage
     plt.title('Distribution of AQI Categories')
     plt.ylabel('')  
 
-    # Creating the legend with both category and percentage since it looks so squished without it. 
+    ## Creating the legend with both category and percentage since it looks so squished without it. 
     legend_labels = [f'{category}: {percentage:.1f}%' for category, percentage in zip(aqi_counts.index, percentages)]
     ax.legend(wedges, legend_labels, title="AQI Categories", loc="center left", bbox_to_anchor=(1, 0.5))
 
@@ -35,12 +37,15 @@ def plot_aqi_pie_chart(file_path):
 
 
 def analyze_average_aqi(file_path):
+    ## reading the data
     data = pd.read_csv(file_path)
+    ## finding the mean of all the countries 
     country_means = data.groupby('country_name')['aqi_value'].mean().sort_values(ascending=False)
-
+    ## gets the top 10 countries and plots
     top_10 = country_means.head(10)
     plt.figure(figsize=(10, 6))
     top_10.plot(kind='bar', color='red', edgecolor='black')
+    ## titles and stuff
     plt.title('Top 10 Countries by Average AQI')
     plt.ylabel('Average AQI')
     plt.xticks(rotation=45, ha='right')
@@ -48,6 +53,24 @@ def analyze_average_aqi(file_path):
     plt.show()
 
 
+def create_boxplot_aqi_by_category(file_path):
+    ## Reading the data
+    df = pd.read_csv(file_path)
+    plt.figure(figsize=(10, 6))
+
+    ## box plot
+    sns.boxplot(x='aqi_category', y='aqi_value', data=df)
+
+
+    plt.title('Distribution of AQI by Category')
+    plt.xlabel('AQI Category')
+    plt.ylabel('AQI Value')
+    
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
 
 plot_aqi_pie_chart(Ourdata)
 analyze_average_aqi(Ourdata)
+create_boxplot_aqi_by_category(Ourdata)
